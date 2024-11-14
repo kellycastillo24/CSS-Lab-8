1d24568175c1bd0c80759c439e42cc6a01195a8d
Kelly <kbc23@njit.edu>
2024-11-14 09:50:59 -0500

    update javascript.js

---
+++ javascript.js
@@ -0,0 +1,57 @@
+//question 1
+var age = 18;
+
+let Name = 'Kelly';
+
+console.log(age);
+console.log(Name);
+
+//question 2
+let a = 100;
+
+let b = 5;
+
+b = b + 5;
+
+console.log(b);
+
+
+//question 3
+let RollNumber = '15'
+if (RollNumber > 10 ){
+    RollNumber = "The RollNumber is greater than 10";
+    console.log(RollNumber);
+}
+
+
+//question 4
+let Member = 'isMember'
+if (Member== "isMember"){
+    Member= "Member discount applied"
+    console.log(Member);
+}
+
+//question 5
+var StartValue = 50;
+ if (StartValue > 0){
+    console.log("Number is positive")
+ }
+
+ else if(StartValue < 0){
+    console.log("Number is negative")
+ }
+ else{console.log("Number is zero")}
+
+ //question 6
+ 
+
+
+ //question 7
+ let FirstName = 'Kelly';
+ let LastName = 'Castillo';
+ let Age = '18'
+console.log("Hello," + FirstName + "" + LastName + "" + ",you are" + Age + "years old.")
+
+//question 8
+
+
